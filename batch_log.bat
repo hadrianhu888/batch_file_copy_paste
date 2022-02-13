f742b510f7788d1d5883ec79574cfa961ab74d6b
hhu1618 <hadrianhu.gm@gmail.com>
2022-02-12 22:43:06 -0500

    better logging and relative addressing

--- batch_log.bat
+++ batch_log.bat
@@ -1,25 +1,28 @@
 @echo off
-set PERL = perl C:\Strawberry\perl\bin\perl.exe
+set PERL = C:\Strawberry\perl\bin\perl5.32.1.exe 
 set LOGFILE=test_log.txt
 call :LOG > %LOGFILE%
 exit /B
 
 :LOG
 
+echo on
+
 echo %date% %time% 
 
 echo %CD%
-set DIR_0 = %CD%/dir0
-set DIR_1 = %CD%/dir1
-set DIR_2 = %CD%/dir2
-set DIR_3 = %CD%/dir3
+set MAIN_PATH = %CD%
+set DIR_0 = %CD%\dir0
+set DIR_1 = %CD%\dir1
+set DIR_2 = %CD%\dir2
+set DIR_3 = %CD%\dir3
 echo %DIR_0%
 echo %DIR_1%
 echo %DIR_2%
 echo %DIR_3%
 
-copy /y %CD%\dir0\txt0.txt %CD%\dir1
-copy /y %CD%\dir1\txt1.txt %CD%\dir0
+copy /y %DIR_0%\txt0.txt %DIR_1%
+copy /y %DIR_1%\txt1.txt %DIR_0%
 
 copy /y %CD%\dir2\txt2.txt %CD%\dir1
 copy /y %CD%\dir1\txt1.txt %CD%\dir2
@@ -31,15 +34,18 @@ copy /y %CD%\dir2\txt0.txt %CD%\dir3
 
 move %CD%\dir0\date_time.txt %CD%
 
+echo %date% %time%
+
+del tempCodeRunnerFile.bat
+
 cd %CD%\dir0
 del /p /a %CD%\dir0\txt1.txt 
 
-cd %CD%\
+cd %CD%
 
 set DateTime = %date% %time%
 echo %date% %time% > %CD%\date_time.txt
 
-cd /d %PERL% 
-call get_time_date
+call get_time_date.pl
 
 echo off
